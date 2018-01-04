d54daf2f4ded74c7e737656304f4fedeb6ed7217
Glitch (fast-case) <none>
2018-01-04 09:09:26 +0000

    🏠🌎 Checkpoint server.js:491517

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get("/search", function(request,response){
     //term:'Four Barrel Coffee',
     location: 'bicol'
   }).then(result => {
-    response.send(JSON.stringify(result).replace(/\\/g, "\n"))
+    response.send(JSON.stringify(result).replace(/\\/g, /\n/))
   }).catch(e => {
     console.log(e);
   });
