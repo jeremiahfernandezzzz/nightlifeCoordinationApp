40a3340d70b9e3007f2c89492de1c096ccb078f7
Glitch (fast-case) <none>
2018-01-04 07:06:22 +0000

    🚏🏟 Checkpoint server.js:491517

--- server.js
+++ server.js
@@ -4,34 +4,82 @@
 // init project
 var express = require('express');
 var app = express();
+var mongodb = require("mongodb")
+var MongoClient = mongodb.MongoClient;
+var url = process.env.DB_URL;
+var session = require('express-session');
+var passport = require('passport')
+  , TwitterStrategy = require('passport-twitter').Strategy;
+//var user = {};
+var mongoose = require('mongoose');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+mongoose.connect(url);
+
+var findOrCreate = require('mongoose-findorcreate')
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({ twitterId: Number});
+UserSchema.plugin(findOrCreate);
+var User = mongoose.model('User', UserSchema);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we're connected!")
+});
+
+
+// Authentication configuration
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'bla bla bla' 
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://cultured-numeric.glitch.me/auth/twitter/callback"
+},
+  function(token, tokenSecret, profile, cb) {
+    User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+      console.log('A new uxer from "%s" was inserted', user.twitterId);
+      return cb(err, user);
+    });
+  }));
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { successRedirect: '/',
+                                     failureRedirect: '/login'}));
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
-const yelp = require('yelp-fusion');
- 
-const client = yelp.client(process.env.apiKey);
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public/views'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/:qwe", function (request, response) { 
-  client.search({
-    term:request.params.qwe,
-    location: 'bicol, philippines'
-  }).then(response => {
-    console.log(response.jsonBody.businesses[0].name);
-  }).catch(e => {
-    console.log(e);
-  });
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-
-app.get("/:qwe", function (request, response) {
-  response.send(request.params.qwe)
-});
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  //console.log(JSON.stringify(request.users));
+  response.send("hi");
+  //response.set
+  
+});
+
+
+
