f4ac2859de1d90c9853869714517d54d318f8625
Glitch (fast-case) <none>
2018-01-04 09:02:26 +0000

    🛥👴 Checkpoint server.js:491517

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get("/search", function(request,response){
     location: 'bicol'
   }).then(response => {
     bus = response;
-    JSON.parse(bus).forEach(function(businesses){
+    JSON.parse(b.forEach(function(businesses){
       response.send(businesses[0].name)
     })
   }).catch(e => {
