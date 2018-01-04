7a6b084a7913d6f758a45d58917315f67fc79f62
Glitch (fast-case) <none>
2018-01-04 08:54:26 +0000

    😥🐔 Checkpoint

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get("/search", function(request,response){
     //term:'Four Barrel Coffee',
     location: 'bicol'
   }).then(response => {
-    bus = JSON.stringify(response).replace(/\n/-g,/\n/);
+    bus = JSON.stringify(response).replace(/\/,/\n/);
     console.log(bus)
   }).catch(e => {
     console.log(e);
